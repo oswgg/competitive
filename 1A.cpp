f047ff07cbd413a099708cecb28b9a5b9c432655
Oswaldo G. <oswaldo.gg0520@gmail.com>
2024-11-26 20:38:03 -0600

    1A comment

--- 1A.cpp
+++ 1A.cpp
@@ -1,6 +1,10 @@
 //
 // Created by Oswaldo G. on 26/11/24.
 //
+/**
+ * En este solo intento cubrir por completo un lado, por lo que redondeo y despues el otro
+ * Asi teniendo cuantos ocupo de cada lado, solamente es obtener el total, sacando el area.
+ */
 #include <iostream>
 #include <cmath>
 #define fastIO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
